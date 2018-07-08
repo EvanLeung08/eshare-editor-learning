c0d2f9476f7289f3026a4a0de3df4d45c8e6d294
Evan43789596 <10856214@163.com>
2018-07-08 10:54:39 +0800

    feat:初始化项目

---
+++ markdown-learning/src/main/java/com/eshare/markdown/demo/MarkdownLearningApplication.java
@@ -0,0 +1,12 @@
+package com.eshare.markdown.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkdownLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkdownLearningApplication.class, args);
+	}
+}
